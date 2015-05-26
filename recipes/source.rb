0417bba1a2e643cc358807bcbd183976c97ab3af
Michael Burns <michael.burns@rackspace.com>
2015-05-26 04:01:50 -0700

    prefer singlequotes

--- recipes/source.rb
+++ recipes/source.rb
@@ -1,4 +1,4 @@
-sslh_url = "http://www.rutschle.net/tech/sslh-v#{node["sslh"]["source"]["version"]}.tar.gz"
+sslh_url = "http://www.rutschle.net/tech/sslh-v#{node['sslh']['source']['version']}.tar.gz"
 src_filepath  = "#{Chef::Config['file_cache_path'] || '/tmp'}/sslh-v#{node['sslh']['source']['version']}.tar.gz"
 src_dirpath = "#{Chef::Config['file_cache_path'] || '/tmp'}/sslh-v#{node['sslh']['source']['version']}"
 install_dirpath = "#{node['sslh']['prefix']}"
@@ -9,11 +9,11 @@ install_dirpath = "#{node['sslh']['prefix']}"
   end
 end
 
-user node["sslh"]["user"] do
+user node['sslh']['user'] do
   action :create
-  home "/var/sslh"
+  home '/var/sslh'
   system true
-  shell "/sbin/nologin"
+  shell '/sbin/nologin'
 end
 
 remote_file sslh_url do
@@ -24,7 +24,7 @@ remote_file sslh_url do
 end
 
 bash 'unarchive_source' do
-  cwd  ::File.dirname(src_filepath)
+  cwd ::File.dirname(src_filepath)
   code <<-EOH
     tar zxf #{::File.basename(src_filepath)} -C #{::File.dirname(src_filepath)}
   EOH
@@ -52,7 +52,7 @@ bash 'install_ssl-select' do
   code <<-EOH
 cp #{src_dirpath}/sslh-select #{install_dirpath}/sbin/
 EOH
-  creates "/usr/local/sbin/sslh-select"
+  creates '/usr/local/sbin/sslh-select'
 end
 
 bash 'install_ssl-init' do
@@ -60,7 +60,7 @@ bash 'install_ssl-init' do
   code <<-EOH
 cp #{src_dirpath}/scripts/etc.rc.d.init.d.sslh.centos /etc/rc.d/init.d/sslh
 EOH
-  creates "/etc/rc.d/init.d/sslh"
+  creates '/etc/rc.d/init.d/sslh'
 end
 
 bash 'install_ssl-default' do
@@ -68,59 +68,56 @@ bash 'install_ssl-default' do
   code <<-EOH
 cp #{src_dirpath}/scripts/etc.default.sslh  /etc/default/sslh
 EOH
-  creates "/etc/default/sslh"
+  creates '/etc/default/sslh'
 end
 
-
-template "/etc/sysconfig/sslh" do
-  owner "root"
-  group "root"
+template '/etc/sysconfig/sslh' do
+  owner 'root'
+  group 'root'
   mode 0755
   action :create
   notifies :restart, 'service[sslh]'
 end
 
-template "/etc/sslh.cfg" do
-  owner "root"
-  group "root"
+template '/etc/sslh.cfg' do
+  owner 'root'
+  group 'root'
   mode 0644
   action :create
   notifies :restart, 'service[sslh]'
 end
 
-template "/etc/rsyslog.d/sslh.conf" do
-  source "rsyslog-sslh.conf.erb"
-  owner "root"
-  group "root"
+template '/etc/rsyslog.d/sslh.conf' do
+  source 'rsyslog-sslh.conf.erb'
+  owner 'root'
+  group 'root'
   mode 0644
   action :create
   notifies :restart, 'service[rsyslog]'
 end
 
-template "/etc/logrotate.d/sslh" do
-  source "logrotate-sslh.erb"
-  owner "root"
-  group "root"
+template '/etc/logrotate.d/sslh' do
+  source 'logrotate-sslh.erb'
+  owner 'root'
+  group 'root'
   mode 0644
   action :create
 end
 
-
-directory "/var/run/sslh" do
-  user "sslh"
-  group "sslh"
+directory '/var/run/sslh' do
+  user 'sslh'
+  group 'sslh'
   mode 0755
   action :create
 end
 
 
 service 'sslh' do
-  supports :status => true, :restart => true
-  action   [:start, :enable]
+  supports status: true, restart: true
+  action [:start, :enable]
 end
 
 service 'rsyslog' do
-  supports :status => true, :restart => true
-  action   [:start, :enable]
+  supports status: true, restart: true
+  action [:start, :enable]
 end
-
